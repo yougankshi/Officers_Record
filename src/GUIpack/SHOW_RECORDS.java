38903c5df1fdf1fe42049c690a398aa4f4b750a4
Yougankshi <yougankshib@gmail.com>
2023-09-25 17:45:58 +0530

    Successful

--- src/GUIpack/SHOW_RECORDS.java
+++ src/GUIpack/SHOW_RECORDS.java
@@ -11,8 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.table.DefaultTableModel;
-import static org.omg.CORBA.AnySeqHelper.id;
-import static org.omg.CORBA.BooleanSeqHelper.id;
 
 /**
  *
